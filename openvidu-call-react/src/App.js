5449e94eff4c7dd09bef0309edccb663076bd193
liuwin7 <liuwin7@163.com>
2021-06-16 18:34:50 +0800

    修改会话的各个状态

--- openvidu-call-react/src/App.js
+++ openvidu-call-react/src/App.js
@@ -4,6 +4,9 @@ import {Phone} from "@material-ui/icons";
 import OutgoingComponent from "./components/call/OutgoingComponent";
 import IncomingComponent from "./components/call/IncomingComponent";
 import VideoRoomComponent from "./components/VideoRoomComponent";
+import { useLocation } from 'react-router-dom';
+import {parse} from 'qs';
+import {useSnackbar} from "notistack";
 
 const useStyles = makeStyles(theme => ({
     fab: {
@@ -15,6 +18,18 @@ const useStyles = makeStyles(theme => ({
 
 const App = () => {
     const classes = useStyles();
+    const location = useLocation();
+    const { enqueueSnackbar } = useSnackbar();
+
+    let mineId = '001', originUser = '张三', peerUserId = '002', peerUser = '李四';
+    if (location.search.length !== 0) {
+        const searchStr = location.search.slice(1);
+        const {userId, toUserId, to, origin} = parse(searchStr);
+        mineId = userId;
+        peerUserId = toUserId;
+        originUser = origin;
+        peerUser = to;
+    }
 
     const [outgoing, setOutgoing] = useState(false);
     const [incoming, setIncoming] = useState(false);
@@ -25,10 +40,11 @@ const App = () => {
         setOutgoing(true);
         const data = {
             type: 'outgoing',
-            to: '张三',
-            toUserId: '001',
-            from: '李四',
-            fromUserId: '002',
+            action: 'ring',
+            to: peerUser,
+            toUserId: peerUserId,
+            origin: originUser,
+            originUserId: mineId,
         };
         socket.send(JSON.stringify(data));
     };
@@ -38,20 +54,46 @@ const App = () => {
         const data = {
             type: 'outgoing',
             action: 'cancel',
-            to: '张三',
-            toUserId: '001',
-            from: '李四',
-            fromUserId: '002',
+            to: peerUser,
+            toUserId: peerUserId,
+            origin: originUser,
+            originUserId: mineId,
         };
         socket.send(JSON.stringify(data));
+        enqueueSnackbar('Cancel', {variant: 'warning', preventDuplicate: true});
     };
 
     const handleReject = () => {
         setIncoming(false);
+        const data = {
+            type: 'incoming',
+            action: 'reject',
+            originUserId: peerUserId,
+        };
+        socket.send(JSON.stringify(data));
+        enqueueSnackbar('Rejected', {variant: 'warning', preventDuplicate: true});
     };
 
     const handleAnswer = () => {
         setIncoming(false);
+        const data = {
+            type: 'incoming',
+            action: 'answer',
+            originUserId: peerUserId,
+        };
+        socket.send(JSON.stringify(data));
+        enqueueSnackbar('Connected', {variant: 'success', preventDuplicate: true});
+    };
+
+    const handleLeaveSession = () => {
+        setSession(undefined);
+        const data = {
+            type: 'incoming',
+            action: 'handoff',
+            peerUserId,
+        };
+        socket.send(JSON.stringify(data));
+        enqueueSnackbar('Disconnected', {variant: 'info', preventDuplicate: true});
     };
 
     // create socket
@@ -62,12 +104,31 @@ const App = () => {
             // register
             const data = {
                 type: 'registration',
-                userId: '001',
+                userId: mineId,
             }
             ws.send(JSON.stringify(data));
         });
         ws.addEventListener('message', ev => {
             console.log('message', ev.data);
+            const messageData = JSON.parse(ev.data);
+            const {type} = messageData;
+            if (type === 'incoming') {
+                const {action} = messageData;
+                if (action === 'ring') {
+                    setIncoming(true);
+                } else if (action === 'cancel') {
+                    setIncoming(false);
+                    enqueueSnackbar('Cancel', {variant: 'warning', preventDuplicate: true});
+                }
+            } else if (type === 'connect') {
+                setOutgoing(false);
+                const {session} = messageData;
+                setSession(session);
+                enqueueSnackbar('Connected', {variant: 'success', preventDuplicate: true});
+            } else if (type === 'disconnect') {
+                setSession(undefined);
+                enqueueSnackbar('Disconnected', {variant: 'info', preventDuplicate: true});
+            }
         });
         setSocket(ws);
     }, []);
@@ -88,6 +149,13 @@ const App = () => {
             {
                 session && <VideoRoomComponent
                     openviduServerUrl="https://192.168.8.181:4443"
+                    sessionName={session}
+                    user={originUser}
+                    leaveSession={handleLeaveSession}
+                    // joinSession={() => {
+                    //     setIncoming(false);
+                    //     setOutgoing(false);
+                    // }}
                 />
             }
         </div>
